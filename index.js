01609404ce3814ef89914163fbcc9707142c54d0
shobhit7390 <vms6402@gmail.com>
2022-07-19 23:48:43 +0530

    Body Parser imported

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var express=require('express')
 var ejs=require('ejs')
+var bodyParser=require('body-parser')
 
 var app=express();
 
@@ -8,6 +9,7 @@ app.use(express.static('public'));
 app.set('view engine','ejs');
 
 app.listen(8080);
+app.use(bodyParser.urlencoded({extended:true}))
 
 // localhost:8080
 app.get('/',function(req,res){

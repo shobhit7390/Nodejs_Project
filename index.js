22d0d34e84d6ddd662bbcb57058ad2b656693036
shobhit7390 <vms6402@gmail.com>
2022-07-23 18:52:42 +0530

    Server created

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 var express=require('express')
 var ejs=require('ejs')
 var bodyParser=require('body-parser')
+var mysql=require('mysql')
+var session=require('express-session')
+
+mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"Nodejs_Project"
+})
 
 var app=express();
 
@@ -10,8 +19,28 @@ app.set('view engine','ejs');
 
 app.listen(8080);
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(session({secret:"secret"}))
 
 // localhost:8080
 app.get('/',function(req,res){
-    res.render('pages/index');
+
+
+    var con=mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        password:"",
+        database:"Nodejs_Project"
+    })
+
+    con.query("SELECT * FROM products",(err,result)=>{
+
+        res.render('pages/index',{result:result});
+
+    })
+
+    
 });
+
+app.post('/add_to_cart',function(req,res){
+    
+})
